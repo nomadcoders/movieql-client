3555bfae14802c39c97a5fdc79e136116fed9a9e
니꼬 <nico@nomadcoders.co>
2022-04-15 18:01:48 +0900

    1.2 ApolloProvider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import client from "./client";
+import { ApolloProvider } from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,28 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return <div>This is a list of movies.</div>;
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+              id
+            }
+          }
+        `,
+      })
+      .then((results) => setMovies(results.data.allMovies));
+  }, [client]);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }
