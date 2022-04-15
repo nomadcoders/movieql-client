92d7078c8423d216692377e8586ab5a74282a7be
니꼬 <nico@nomadcoders.co>
2022-04-15 17:53:50 +0900

    1.0 Setup

---
+++ src/client.js
@@ -0,0 +1,20 @@
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      {
+        allMovies {
+          title
+        }
+      }
+    `,
+  })
+  .then((data) => console.log(data));
+
+export default client;
