385e642643d31dc010c62ee973b47a2455e2f4c4
니꼬 <nico@nomadcoders.co>
2022-04-18 17:38:21 +0900

    2.0 Styles

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,14 +1,55 @@
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($movieId: ID!) {
     movie(id: $movieId) {
       id
       title
-      small_cover_image
+      medium_cover_image
+      rating
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
 `;
 
 export default function Movie() {
@@ -18,9 +59,13 @@ export default function Movie() {
       movieId: id,
     },
   });
-  console.log(data, loading);
-  if (loading) {
-    return <h1>Fetching movie...</h1>;
-  }
-  return <div>{data.movie.title}</div>;
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,31 +1,91 @@
+import styled from "styled-components";
 import { gql, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
 const ALL_MOVIES = gql`
   query getMovies {
     allMovies {
-      title
       id
+      title
+      medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+const PosterContainer = styled.div`
+  height: 400px;
+  border-radius: 7px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
+
+const PosterBg = styled.div`
+  background-image: url(${(props) => props.background});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
 export default function Movies() {
-  const { data, loading, error } = useQuery(ALL_MOVIES);
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-  if (error) {
-    return <h1>Could not fetch :(</h1>;
-  }
+  const { data, loading } = useQuery(ALL_MOVIES);
   return (
-    <ul>
-      <h1>Movies</h1>
-      {data.allMovies.map((movie) => (
-        <li key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <Container>
+      <Header>
+        <Title>Apollo Movies</Title>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <MoviesGrid>
+        {data?.allMovies?.map((movie) => (
+          <PosterContainer key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <PosterBg background={movie.medium_cover_image} />
+            </Link>
+          </PosterContainer>
+        ))}
+      </MoviesGrid>
+    </Container>
   );
 }
